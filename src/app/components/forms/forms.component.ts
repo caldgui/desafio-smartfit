364eb90b0fcd268e017a53a87276e621c8947063
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2024-01-20 01:34:21 -0300

    Extilização Forms Component

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+  public results = [];
+
+  constructor() {}
+}
