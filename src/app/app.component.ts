873fca551f283d37d38468b72978e9c6a0195285
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2024-01-22 15:21:04 -0300

    Responsividade Formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from './components/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule]
 })
 export class AppComponent {
   title = 'desafio-smartfit';
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
 
   public results = [];
+  public formGroup!: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  public onSubmit(){
+    console.log(this.formGroup.value);
+  }
+
+  public onClean(){
+    this.formGroup.reset();
+  }
 }
