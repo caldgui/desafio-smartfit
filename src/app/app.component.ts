8861d24d9a04865305d3a9324c430560bd1f6230
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2024-01-22 15:57:02 -0300

    Criação get all units

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from './components/forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule, HttpClientModule]
 })
 export class AppComponent {
   title = 'desafio-smartfit';
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitService } from '../../services/get-unit.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
@@ -13,9 +15,13 @@ export class FormsComponent implements OnInit{
   public results = [];
   public formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder,
+              private getUnitService: GetUnitService) {}
 
   ngOnInit(): void {
+    this.getUnitService.getAllUnits().subscribe((response) => {
+      console.log(response)
+    });
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false
